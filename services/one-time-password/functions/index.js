e6d33b5701dbaede1a372d21477dbde0a145a3e1
Piyush Santwani <piyush.santwani@bookmyshow.com>
2017-11-02 15:59:14 +0530

    firebase functions for OTP

--- services/one-time-password/functions/index.js
+++ services/one-time-password/functions/index.js
@@ -1,9 +1,9 @@
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
-const createUser = require('./functions/create_user');
-const serviceAccount = require('./config/service_account.json');
-const requestOneTimePassword = require('./functions/request_one_time_password');
-const verifyOneTimePassword = require('./functions/verify_one_time_password');
+const createUser = require('./create_user');
+const serviceAccount = require('./service_account.json');
+const requestOneTimePassword = require('./request_one_time_password');
+const verifyOneTimePassword = require('./verify_one_time_password');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
--- services/one-time-password/functions/request_one_time_password.js
+++ services/one-time-password/functions/request_one_time_password.js
@@ -1,16 +1,17 @@
 const admin = require('firebase-admin');
-const twilio = require('../config/twilio');
+const twilio = require('./twilio');
 
 module.exports = function(req, res) {
   if (!req.body.phone) {
     return res.status(422).send({ error: 'You must provide a phone number' });
   }
 
-  const phone = String(req.body.phone).replace(/[^\d]/g, '');
+  //const phone = String(req.body.phone).replace(/[^\d]/g, '');
+  const phone = String(req.body.phone);
 
   admin.auth().getUser(phone)
     .then(userRecord => {
-      const code = Math.floor((Math.random() * 8999 + 1000));
+      const code = Math.floor((Math.random() * 8999 + 1000));      
 
       twilio.messages.create({
         body: 'Your code is ' + code,
