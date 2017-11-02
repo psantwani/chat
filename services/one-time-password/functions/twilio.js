62a5f415e0a8b0b0352a75395be66d5adbbfe36e
Piyush Santwani <piyush.santwani@bookmyshow.com>
2017-11-02 16:04:42 +0530

    deleted file

--- services/one-time-password/functions/twilio.js
+++
@@ -1,6 +0,0 @@
-const twilio = require('twilio');
-
-const accountSid = 'ACffbc1804f9569b9b66c1211bab97d38a';
-const authToken = '940e0f93357eaf6a240cf415e2daadc4';
-
-module.exports = new twilio.Twilio(accountSid, authToken);
